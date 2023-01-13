bf9da46caaceadbaa198a841dc2a224769b8d976
erickhansx <erickhansx@gmail.com>
2023-01-12 21:10:25 -0600

    Create method fizzbuzz in Solver

--- solver.rb
+++ solver.rb
@@ -10,6 +10,15 @@ class Solver
   def self.reverse(str)
     str.reverse
   end
+  def self.fizzbuzz(num)
+    case
+    when num % 15 == 0 then 'fizzbuzz'
+    when num % 3 == 0 then 'fizz'
+    when num % 5 == 0 then 'buzz'
+    else
+      num.to_s
+    end
+  end
 end
 
-puts Solver.reverse('Erick')
+puts Solver.fizzbuzz(15)
